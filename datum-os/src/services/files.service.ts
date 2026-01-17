a179c19ee98c4b4f52ff4ab2f202be2989fc4e28
falgren1010 <lu.weber97@web.de>
2026-01-17 16:37:22 +0100

    create business-logic layer with dummy functions

---
+++ datum-os/src/services/files.service.ts
@@ -0,0 +1,14 @@
+
+export class FilesService{
+
+    constructor() {
+    }
+
+    uploadFile(): void {
+        // todo implement
+    }
+
+    getFile(): void {
+        // todo implement
+    }
+}
---
+++ datum-os/src/services/orders.service.ts
@@ -0,0 +1,18 @@
+
+export class OrdersService{
+
+    constructor() {
+    }
+
+    createOrder():void {
+        // todo implement
+    }
+
+    getOrder(): void {
+        // todo implement
+    }
+
+    processPayment(): void {
+        // todo implement
+    }
+}
