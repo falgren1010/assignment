88210ac158b706a3d4b29f06c595462f27d2554a
falgren1010 <lu.weber97@web.de>
2026-01-17 17:25:52 +0100

    introduce adapter layer and implement business logic for material service (testing purposes-not final)

---
+++ datum-os/src/adapters/quotes.adapter.ts
@@ -0,0 +1,13 @@
+
+export class QuotesAdapter{
+
+    constructor() {
+    }
+
+    createQuote(): void{
+        throw new Error("not implemented")
+    }
+    getQuote(): void{
+        throw new Error("not implemented")
+    }
+}
