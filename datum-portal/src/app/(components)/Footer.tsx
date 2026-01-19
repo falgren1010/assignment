72cee916186eadee3c503f7c59c4260d5675962d
falgren1010 <lu.weber97@web.de>
2026-01-19 19:27:59 +0100

    create base-pages and import api-contract

---
+++ datum-portal/src/app/(components)/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer className="footer footer-center p-4 bg-neutral text-base-100">
+        <aside>
+                <p>© {new Date().getFullYear()} SAEKI 3D – All rights reserved</p>
+            </aside>
+        </footer>
+    );
+}
